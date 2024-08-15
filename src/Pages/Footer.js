51a02925719bcf98050a1e76b894ea5bba7cdbb9
Yemezzy <ayubayelmee222@gmail.com>
2024-08-15 03:16:14 +0100

    first commit

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -6,23 +6,23 @@ import {  FaLocationCrosshairs, FaTelegram } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div> <footer className='md:mt-40 mt-20 md:px-10 px-2 bbb bg-[#0C1B2E] text-white'>
-    <div className='grid md:grid-cols-3 text-lg font-medium md:py-10'>
+    <div> <footer className='md:mt-40 mt-20 md:px-10 px-5 bbb bg-[#0C1B2E] text-white'>
+    <div className='grid md:grid-cols-3 md:text-lg text-sm font-medium md:py-10'>
         <section>
-            <p className='text-2xl text-[#c11425] font-bold mb-2 md:mb-5 mt-5 md:mt-0'>Address</p>
+            <p className='md:text-2xl text-xl text-[#c11425] font-bold mb-2 md:mb-5 mt-5 md:mt-0'>Address</p>
             <p className='flex items-center gap-2'><FaLocationCrosshairs/>123 Street, New York, USA</p>
             <p className='flex items-center gap-2 mt-3'><FaPhoneAlt/>+012 345 67890</p>
             <p className='flex items-center gap-2 mt-3'><HiOutlineMail/>info@example.com</p>
         </section>
         <section>
-        <p className='text-2xl text-[#c11425] font-bold mb-2 md:mb-5 mt-5 md:mt-0'>Services</p>
+        <p className='md:text-2xl text-xl text-[#c11425] font-bold mb-2 md:mb-5 mt-5 md:mt-0'>Services</p>
             <p className='flex items-center gap-2'><FaDotCircle/> Air Freight</p>
             <p className='flex items-center gap-2 mt-3'><FaDotCircle/> Sea Freight</p>
             <p className='flex items-center gap-2 mt-3'><FaDotCircle/> Road Freight</p>
             <p className='flex items-center gap-2 mt-3'><FaDotCircle/> Courier Freight</p>
         </section>
         <section>
-        <p className='text-2xl text-[#c11425] font-bold mb-2 md:mb-5 mt-5 md:mt-0'>Newsletter</p>
+        <p className='md:text-2xl text-xl text-[#c11425] font-bold mb-2 md:mb-5 mt-5 md:mt-0'>Newsletter</p>
         <p>Be the first to know about new services, special promotions, and exclusive offers tailored to your logistics need. 
             Join our mailing list and stay connected with the latest development in the world of logistics!</p>
             <form action="" className='mt-2 md:flex text-black'>
